57739ffd970866117a2f7be06714397c527dfbff
JeanEric <ericjohn415@gmail.com>
2021-04-24 14:47:40 +0200

    HEroku config files and updates

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = SECRET_KEY = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
